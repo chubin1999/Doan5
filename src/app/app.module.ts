0ef279e768d9ff026dedf5f701adffed458a13ae
chubin1999 <phachgia1999@gmail.com>
2020-09-16 16:23:06 +0700

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { Routes, RouterModule } from '@angular/router';
+import { DanhmucComponent } from './danhmuc/danhmuc.component';
+const routes: Routes = [
+  // {
+  //   path: 'chitiet',
+  //   component: ChitietComponent,
+  // },
+  {
+    path: 'danhmuc',
+    component: DanhmucComponent,
+  },
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path: 'home',
+    component: MainComponent,
+  },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    DanhmucComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),//GỌi đến router
     AppRoutingModule
   ],
   providers: [],
