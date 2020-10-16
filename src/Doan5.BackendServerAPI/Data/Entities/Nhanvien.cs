77ae3d79f1cb598ae023ed6eb179a555fdfa50b1
chubin1999 <phachgia1999@gmail.com>
2020-10-16 21:27:21 +0700

    upload

---
+++ src/Doan5.BackendServerAPI/Data/Entities/Nhanvien.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doan5.BackendServerAPI.Data.Entities
+{
+    public class Nhanvien
+    {
+    }
+}
