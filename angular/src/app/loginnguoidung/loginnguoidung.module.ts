9bae816ae4f390c3681ea9a9899fd72a798cc847
chubin1999 <phachgia1999@gmail.com>
2020-10-19 22:47:43 +0700

    upload

--- angular/src/app/loginnguoidung/loginnguoidung.module.ts
+++ angular/src/app/loginnguoidung/loginnguoidung.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginnguoidungComponent } from './loginnguoidung.component';
+import { LoginnguoidungRoutingModule } from './loginnguoidung-routing.module';
 
 
 
 @NgModule({
   declarations: [LoginnguoidungComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    LoginnguoidungRoutingModule
   ]
 })
 export class LoginnguoidungModule { }
